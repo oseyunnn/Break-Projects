bebdf8726de565922161761010f9dafe1a2b4a5b
Angela Jahziel Encabo <angelajahziel.encabo@cit.edu>
2026-01-03 23:20:40 +0800

    test

---
+++ WallOFWins/backend/main.java
@@ -0,0 +1,8 @@
+package WallOFWins.backend;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Wall of Wins!");
+        // Initialize game components here
+    }
+}
